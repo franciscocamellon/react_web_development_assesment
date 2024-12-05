db3ed392eccebae381c1b0911d9a426c2ede0ff1
franciscocamellon <franciscocamellon@gmail.com>
2024-12-04 23:26:04 -0300

    Initial code

---
+++ src/components/button/index.tsx
@@ -0,0 +1,25 @@
+import { Button, ButtonProps } from "@mui/material";
+
+interface IButtonProps extends ButtonProps {
+  children?: React.ReactNode;
+  variant?: "text" | "contained" | "outlined";
+  size?: "small" | "medium" | "large";
+  className?: string;
+}
+
+const ButtonComponent: React.FC<IButtonProps> = ({ children, ...props }) => {
+  const { variant = "contained", size = "large" } = props;
+
+  return (
+    <Button
+      className={`general-button ${props.className ? props.className : ""}`}
+      variant={variant}
+      size={size}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default ButtonComponent;
---
+++ src/components/grid/index.tsx
@@ -0,0 +1,11 @@
+import { Grid2, Grid2Props } from "@mui/material";
+
+interface IGridProps extends Grid2Props {
+  children?: React.ReactNode;
+}
+
+const GridComponent: React.FC<IGridProps> = ({ children, ...props }) => {
+  return <Grid2 {...props}>{children}</Grid2>;
+};
+
+export default GridComponent;
